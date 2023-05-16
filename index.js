8ce6337804304537d68e2c58d8bc77bad49c35ef
Ronald Heimert <r.heimert@gmail.com>
2023-05-16 15:28:29 +0200

    sync

--- index.js
+++ index.js
@@ -15,6 +15,24 @@ const maxPage = 1;
 const page = 1;
 const searchQuery = "";
 
+const cardData = await fetchCharacters();
 const card = createCharacterCard();
 
 cardContainer.append(card);
+
+async function fetchCharacters() {
+  try {
+    const response = await fetch("https://rickandmortyapi.com/api/character");
+
+    if (response.ok) {
+      const data = await response.json();
+      return data.results;
+    } else {
+      console.error("response is not ok!");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+console.log(cardData);
